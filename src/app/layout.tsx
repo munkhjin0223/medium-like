e92218cc0352f63e3510a2f312ca6eadd70ee245
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2023-07-05 12:18:52 +0800

    add html & body tags for root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import Header from '@/components/Header';
 import './globals.css';
 import siteMetadata from '@/data/siteMetadata';
 
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
 export const metadata = {
   title: siteMetadata.title,
   description: siteMetadata.description,
@@ -9,11 +13,15 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'>
-      <div className='flex h-screen flex-col justify-between'>
-        <Header />
-        <main className='mb-auto'>{children}</main>
-      </div>
-    </div>
+    <html lang='en'>
+      <body className={inter.className}>
+        <div className='mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'>
+          <div className='flex h-screen flex-col justify-between'>
+            <Header />
+            <main className='mb-auto'>{children}</main>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
